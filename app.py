ba49e568cb0d83cad8067721c15e898c4091f209
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-08-23 17:50:47 +0100

    added the routes

--- app.py
+++ app.py
@@ -15,54 +15,67 @@ migrate = Migrate(app, db)
 
 # Connect sql db to heroku
 
-# class Model
+
 class URLModel(db.Model):
     __tablename__ = 'urls'
 
-    id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.string())
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    url = db.Column(db.String())
     short_id = db.Column(db.String())
 
     def __init__(self, url, short_id):
         self.url = url
         self.short_id = short_id
 
-    def __repr__(self) -> str:
-        return f"<URL: {self.url}"
+    def __repr__(self):
+        return f"<URL:  {self.url}>"
+
 
 db.create_all()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def url_handler():
     if request.method == 'POST':
         url = request.form['url']
         check_url = URLModel.query.filter_by(url=url).first()
-        
+
         if not url:
-            return render_template('index.html',text='You need to enter a URL!'), 200
-        
+            return render_template('index.html', text='You need to enter a URL!'), 200
+
         if check_url:
-            short_id=check_url.short_id
-            return render_template('index.html',text='That URL already has a link:', link=request.host_url + short_id), 200
-       
+            short_id = check_url.short_id
+            return render_template('index.html', text='That URL already has a link:', link=request.host_url + short_id), 200
+
         short_id = create_short_id(6)
 
         new_url = URLModel(
             url=url, short_id=short_id)
         db.session.add(new_url)
         db.session.commit()
-        
 
-        return render_template('index.html', text= "Here is your new URL:", link=request.host_url + short_id)
-         
+        return render_template('index.html', text="Here is your new URL:", link=request.host_url + short_id)
+
     return render_template('index.html')
 
-
+# id route
+@app.route('/<short_id>')
+def redirect_url(short_id):
+    link = URLModel.query.filter_by(short_id=short_id).first()
+    if link:
+        return redirect(link.url)
+    else:
+        return render_template('index.html', text=f"That link doesn't exist, please create one here")
+        
+# short id generator        
+def create_short_id(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 # Error handling
 @app.errorhandler(404)
 def handle_404(err):
     return render_template('errors/404.html'), 404
+
 
 @app.errorhandler(405)
 def handle_405(err):
@@ -72,6 +85,7 @@ def handle_405(err):
 @app.errorhandler(500)
 def handle_500(err):
     return render_template('errors/500.html'), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
