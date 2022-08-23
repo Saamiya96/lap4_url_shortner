603887e0db1567d37e6bcc52f8adb51d78a27bdd
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-08-23 17:34:04 +0100

    added models

--- app.py
+++ app.py
@@ -2,11 +2,35 @@ from crypt import methods
 from operator import methodcaller
 from flask import Flask, jsonify, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_cors import CORS
 from werkzeug import exceptions
 
 app = Flask(__name__)
 CORS(app)
+
+# Database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Connect sql db to heroku
+
+# class Model
+class URLModel(db.Model):
+    __tablename__ = 'urls'
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.string())
+    short_id = db.Column(db.String())
+
+    def __init__(self, url, short_id):
+        self.url = url
+        self.short_id = short_id
+
+    def __repr__(self) -> str:
+        return f"<URL: {self.url}"
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
