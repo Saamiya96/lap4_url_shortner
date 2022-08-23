86d992cfe55cad767bb0910075251fcfcb2d2245
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-08-23 16:57:20 +0100

    no changes

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from werkzeug import exceptions
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return ("Hi")
+
+if __name__ == "__main__":
+    app.run(debug=True)
