e36938b6ab6a20b342ba9bccc13549b96d57a28c
saamiya96 <saamiyayousuf@hotmail.co.uk>
2022-08-23 17:19:11 +0100

    added error handling

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from crypt import methods
+from operator import methodcaller
 from flask import Flask, jsonify, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -6,9 +8,25 @@ from werkzeug import exceptions
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template ('index.html')
+
+
+
+# Error handling
+@app.errorhandler(404)
+def handle_404(err):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(405)
+def handle_405(err):
+    return render_template('errors/405.html'), 405
+
+
+@app.errorhandler(500)
+def handle_500(err):
+    return render_template('errors/500.html'), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
